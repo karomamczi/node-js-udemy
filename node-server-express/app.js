6b4ff76122f6abda633b764e0210ee04bdf99719
Karolina Mamczarz <karomamczi@gmail.com>
2020-05-10 23:19:29 +0200

    feat: Routes

--- node-server-express/app.js
+++ node-server-express/app.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware!');
+app.use('/', (req, res, next) => {
+  console.log('This always runs');
   next();
+})
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<h1>Add product</h1>');
 });
 
-app.use((req, res, next) => {
-  console.log('In another middleware!');
+app.use('/', (req, res, next) => {
   res.send('<h1>Hello from Express!</h1>');
 });
 
