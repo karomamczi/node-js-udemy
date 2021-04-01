9d801035190c62906af4dbcfbf67bc4b4032e07a
Karolina Mamczarz <karomamczi@gmail.com>
2021-04-01 22:55:09 +0200

    feat: Root dir helper

--- node-server-express/app.js
+++ node-server-express/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const rootDir = require('./util/path');
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
